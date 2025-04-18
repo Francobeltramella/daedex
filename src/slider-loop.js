af94fbbb81d4da56fd2faab33b704564d622ee5c
Franco <franbeltramella@hotmail.com>
2025-04-17 22:01:20 -0300

    build config Final Font

--- src/slider-loop.js
+++ src/slider-loop.js
@@ -215,7 +215,7 @@ document.querySelectorAll('a[data-shader]').forEach(button => {
 });
 
 // === Crear imágenes y textos ===
-fontLoader.load('http://localhost:5173/src/orbitron.json', (font) => {
+fontLoader.load('https://3dlive.netlify.app/ORBITRON.json', (font) => {
   images.forEach((img, idx) => {
     const texture = loader.load(img.src);
     const uniforms = {
