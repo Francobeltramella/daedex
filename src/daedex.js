98538712eaa20dd0e67a26c59c3509057b34e9e3
Franco <franbeltramella@hotmail.com>
2025-08-28 15:43:15 -0300

    wireframe animation 5

--- src/daedex.js
+++ src/daedex.js
@@ -302,13 +302,13 @@ document.addEventListener("DOMContentLoaded", () => {
         { rx: 0,    ry: -6,   x: 0, duration: 1.2 },  
         { rx: 10,   ry: 15,   x: 0, duration: 1.5 },  
         { rx: 8,   ry: -12,  x: 0, duration: 1.5 },   
-        { rx: 5,    ry: 25,   x: 0, duration: 1.4 }      
+        { rx: 0,    ry: 0,   x: 0, duration: 1.4 }      
     ],
     "glb4": [
         { rx: 0,    ry: -6,   x: 0, duration: 1.2 },  
         { rx: -90,  ry: 0,   x: 0,y: -1, duration: 1.5 },
         { rx: 60,   ry: 25,  x: 0, duration: 1.5 },   
-        { rx: 0,    ry: -6,   x: 0, duration: 1.2 }      
+        { rx: 0,    ry: 0,   x: 0, duration: 1.2 }      
     ]
     };
   
